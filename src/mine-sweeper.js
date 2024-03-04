bb39e1bd075bd240dc209bad037d944c53cc63a2
Grisha <36422717+webdevgrisha@users.noreply.github.com>
2024-03-04 09:33:07 +0100

    feat: resolve mine-sweeper task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const rowLen = matrix[0].length;
+  const rowCount = matrix.length;
+  return matrix.map((row, rowIndex, matrix) => {
+    return row.map((cell, cellIndex) => {
+      if (cell) return 1;
+
+      let minesCount = 0;
+
+      const topPos = rowIndex - 1;
+      const rightPos = cellIndex + 1;
+      const bottomPos = rowIndex + 1;
+      const leftPos = cellIndex - 1;
+
+      // top
+      if(topPos >= 0) {
+        const start = cellIndex - 1 < 0 ? 0 : cellIndex - 1;
+        const end = cellIndex + 1 >= rowLen ? rowLen - 1 : cellIndex + 1;
+        for(let i = start; i <= end; i++) {
+          minesCount += +matrix[topPos][i];
+        }
+      }
+
+      // right
+      if(rightPos < rowLen) {
+        minesCount += +matrix[rowIndex][rightPos];
+      }
+
+      // bottom
+      if(bottomPos < rowCount) {
+        const start = cellIndex - 1 < 0 ? 0 : cellIndex - 1;
+        const end = cellIndex + 1 >= rowLen ? rowLen - 1 : cellIndex + 1;
+        for(let i = start; i <= end; i++) {
+          minesCount += +matrix[bottomPos][i];
+        }
+      }
+
+      // left
+      if(leftPos >= 0) {
+        minesCount += +matrix[rowIndex][leftPos];
+      }
+
+      return minesCount;
+    });
+  });
 }
 
 module.exports = {
